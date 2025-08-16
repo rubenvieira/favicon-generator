51a9e3acbc6172c769d396401ed7add0f9b142a2
[dyad] <rubenv@gmail.com>
2025-08-16 11:28:26 -0700

    [dyad] Fixing router basename for subdirectory - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tools/favicon-generator/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
